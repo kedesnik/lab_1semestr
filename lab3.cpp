efd3a997f61d9c3dcd2315712f9b6921d5c99c73
Egor Erpulev <kedesnik@MacBook-Pro-Egor.local>
2024-12-02 02:29:14 +0300

    new variable added

--- lab3.cpp
+++ lab3.cpp
@@ -34,18 +34,20 @@ double Task7_1(int n, float x);
 void Task8();
 int Task8_1(int n);
 int Task8_2(int n);
+unsigned int fibb(unsigned short n);
 
 void Task9();
 int M(int n);
-
+int Task9_1(int n);
 
 
 int main()
 {
 	setlocale(LC_ALL, "");
 	int n;
-	cout<<"Какую задачу?";cin>>n;
+	cout<<"Какую задачу? ";cin>>n;
 	switch (n) {
+		
 case 1:
 	Task1();
 	break;
@@ -74,7 +76,7 @@ case 9:
 	Task9();
 	break;
 }
-	Task5();
+	//Task5();
 }
 
 void Task1() {
@@ -151,6 +153,7 @@ void Task5() {
 	cout << "Введите число x "; cin >> x;
 	cout << "Введите число eps "; cin >> eps;
 	cout << "Введите число "; cin >> n;
+	//int n=0;
 	float x1 = Task5_1(n, x, eps);
 	float y = 1, a = 0;
 	while (abs(y - a) >= eps) {
@@ -159,6 +162,7 @@ void Task5() {
 	}
 	cout << "y = " << y;
 }
+
 float Task5_1(int n, float x, float eps) {
 	if (n == 1) return 1;
 	else return 0.5 * (Task5_1(n - 1, x, eps) + x / Task5_1(n - 1, x, eps));
@@ -169,12 +173,10 @@ void Task6() {
 	cout << "Введите Xнач Хкон dX: "; cin >> xn >> xk >> xd;
 	float a, b, c;
 	cout << "Введите a b c: "; cin >> a >> b >> c;
-	Task6_1(xn, xk, xd, a, b, c);
-}
-void Task6_1(float xn, float xk, float xd, float a, float b, float c) {
+	
 	int a1 = int(a), b1 = int(b), c1 = int(c);
 	float x = xn;
-	int ee = (a1 | b1) & ((a1 | c1));
+	int ee = (((a1 && b1)^c1)!=0);
 	while (x <= xk) {
 		if (ee)
 			cout << setw(10)<<x << setw(10) << Task6_2(x, a, b, c) << endl;
@@ -184,27 +186,26 @@ void Task6_1(float xn, float xk, float xd, float a, float b, float c) {
 	}
 }
 float Task6_2(float x, float a, float b, float c) {
-	if (x + 10 < 0 && b != 0)
-		return a * x * x - c * x + b;
-	else if (x + 10 > 0 && b == 0)
-		return (x - a) / (x - c);
-	else
-		return -x / (a - c);
+	if (x<1 && c != 0)
+        return a*pow(x,2)+(b/c);
+    else if (x>15 && c==0)
+        return (x-a)/pow(x-c,2) ;
+    return x*x+c*c;
 }
 
 void Task7() {
 	float x;
 	int n;
-	do {
-		cout << "Введите х (0<x<=2) n: "; cin >> x >> n;
-	} while (x <= 0 || x > 2);
-	double lnx = Task7_1(n,x);
-	cout << "Натуральный логарифм будет равен " << lnx;
+	do { 
+		cout << "Введите |х|>1  n: "; cin >> x >> n;
+	} while (fabs(x)==0);
+	double arth = Task7_1(n,x);
+	cout << "Гиперболический тангенс будет равен " << arth;
 }
 double Task7_1(int n, float x) {
 	double y = 0;
 	for (int i = 0; i <= n; i++) {
-		y += pow(-1, n) * pow(x - 1, n + 1) / (n + 1);
+		y += 1 / ((2 * n + 1) * pow(x,(2 * n + 1)));
 	}
 	return y;
 }
@@ -213,7 +214,9 @@ void Task8() {
 	int a;
 	cout << "Введите число "; cin >> a;
 	cout << Task8_1(a);
+	
 }
+
 int Task8_1(int n) {
 	if (n == 0)return 0;
 	else if (n == 1)return 1;
@@ -229,151 +232,21 @@ int Task8_2(int n) {
 	return a[n];
 }
 
+
 void Task9() {
-	int n;
-	cout << "Введите число "; cin >> n;
-	cout << M(n) << endl << endl;
-
-
-	while (n < 100) n += 11;
-	cout << n - 10;
+	int a[10], n, i;  
+	cout << "Введите число "; 
+	cin >> n;
+       
+	for(i=0; n>0; i++)    
+	{    
+		a[i]=n%2;    
+		n= n/2;  
+	}    
+	cout<<"Двоичный вид числа = ";    
+	for(i=i-1 ;i>=0 ;i--)    
+	{    
+		cout<<a[i];    
 }
-int M(int n) {
-	if (n >= 100) return n - 10;
-	else return M(n + 11);
-}
-
-
-
-/*
-//--------------------------------------------------------------------
-#include <iostream>
-#include <iomanip>
-
-using namespace std;
-
-void MakeArrInt(int* mass, int n);
-void MakeArrDouble(double* mass, int n);
-void ShowArrInt(int* mass,int n);
-void ShowArrDouble(double* mass,int n);
-void Task1();
-void Task2();
-void Task3();
-void Task4();
-double SumElEvDouble(double* mass, int n);
-int MultIntAfterMax(int* mass, int n);
-
-int main()
-{
-    srand(unsigned(time(0)));
-    cout << "*******************************" << endl;
-    cout<<setw(45)<<"Лабораторная работа №4"<<endl;
-    cout<<setw(40)<<"Одномерные массивы"<<endl;
-    cout<<"Выполнил студент группы 151412"<<endl;
-    cout<<setw(49)<<"Пирогов Павел Алексеевич"<<endl;
-    cout<<setw(30)<<"Вариант №17"<<endl;
-    cout << "*******************************" << endl<<endl;
-    int n;
-    cout<<"Какую задачу хотите запустить? ";cin>>n;
-    switch(n){
-    case 1:
-        Task1();
-        break;
-    case 2:
-        Task2();
-        break;
-    case 3:
-        Task3();
-        break;
-    case 4:
-        Task4();
-        break;
-    default:
-        cout<<"Такой задачи нет((";
-        break;
-    }
 }
 
-void Task1(){
-    int n;
-    do{
-        cout<<"Сколько чисел хотите в массиве? ";cin>>n;
-    }while(n<=0);
-    int* mass = new int[n];
-    MakeArrInt(mass,n);
-    ShowArrInt(mass,n);
-    delete[] mass;
-}
-
-void MakeArrInt(int* mass, int n){
-    for (int i = 0;i<n;i++){
-        mass[i] = rand()%200-100;
-    }
-}
-
-void MakeArrDouble(double* mass, int n){
-    for (int i = 0;i<n;i++){
-        mass[i] = 0.01 * (rand()%200-100);
-    }
-}
-
-void ShowArrInt(int* mass, int n){
-    for (int i = 0; i<n;i++){
-        cout<<mass[i]<<" ";
-    }
-    cout<<endl;
-}
-void ShowArrDouble(double* mass, int n){
-    for (int i = 0; i<n;i++){
-        cout<<mass[i]<<" ";
-    }
-    cout<<endl;
-}
-
-void Task2(){
-    int n;
-    do{
-        cout<<"Сколько чисел хотите в массиве? ";cin>>n;
-    }while(n<=0);
-    double* mass = new double[n];
-    MakeArrDouble(mass,n);
-    ShowArrDouble(mass,n);
-}
-
-void Task3(){
-    int n;
-    do{
-        cout<<"Сколько чисел хотите в массиве? ";cin>>n;
-    }while(n<=0);
-    double* mass = new double[n];
-    MakeArrDouble(mass, n);
-    ShowArrDouble(mass,mass[0]);
-    cout<<SumElEvDouble(mass,mass[0]);
-}
-
-double SumElEvDouble(double* mass, int n){
-    double summ = 0.0;
-    for (int i = 0;i<n+1;i++){
-        if (i%2!=0)summ += mass[i];
-    }
-    return summ;
-}
-
-void Task4(){
-    //int* mass = MakeArrInt();
-    //ShowArrInt(mass,mass[0]);
-    //cout<<MultIntAfterMax(mass,mass[0]);
-}
-
-int MultIntAfterMax(int* mass, int n){
-    int maxx = mass[1];
-    for(int i = 1;i<n+1;i++){
-        if (maxx < mass[i])maxx = i;
-    }
-    int mult = 1;
-    for(int i = maxx+1;i<=n+1;i++){
-        mult *= mass[i];
-    }
-    return mult;
-}
-*/
--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
 using namespace std;
-
+float f6(float a, float b, float c, float x);
 int main()
 {
-    int n=4;
-    cout << n;
-    return 0;
+  float a, b, c, xn, xk, xd;
+  cout<<"Enter 6 numbers\n";
+  cin>>a>>b>>c>>xn>>xk>>xd;
+  int ac=a;
+  int bc=b;
+  int cc=c;
+
+  while (xn<=xk) {
+    if (((ac && bc)^cc)!=0)
+        cout<<xn<<" - "<<f6(a,b,c,xn)<<endl;
+    else
+        cout<<xn<< " - "<<floor(f6(a,b,c,xn))<<endl;
+    xn+=xd;
+
+  }
+}
+float f6(float a, float b, float c, float x){
+    if (x<1 && c != 0)
+        return a*pow(x,2)+(b/c);
+    if (x>15 && c==0)
+        return (x-a)/pow(x-c,2) ;
+    return x*x+c*c;
 }
